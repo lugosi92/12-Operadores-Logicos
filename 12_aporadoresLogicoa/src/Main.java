d6c2bb667d1622796cd3071e4a0ee72d0de86e4a
khaled___beno <khaled___beno@localhost>
2023-12-26 16:55:08 +0100

    Operadores_logicos

---
+++ 12_aporadoresLogicoa/src/Main.java
@@ -0,0 +1,43 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Operadores logicos
+		//< menor que
+		//> mayor que
+		// == igualdad
+		// != diferente de
+		//<= menor o igual
+		//>= mayor o igual
+		
+		
+		//devuelve dos tipos de valores
+		//true
+		//false
+		
+		/*1 < 5 = true
+		1 > 5 = false
+		1 == 5 = false
+		1 != 5 true
+		1 <= 5 = true
+		1 >= 5 = false*/
+		
+		
+		if(1 < 5) {
+			System.out.println("TRUE");
+		}else{
+			System.out.println("FALSE");
+			
+		}
+	}
+}
+		
+		
+	
+		
+		
+	
+
+
